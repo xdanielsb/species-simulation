b493e0717dbbe7922e1adf8749fa0056e7ca65a9
Zheng LI <z18li@pc-df-705.priv.enst-bretagne.fr>
2019-12-09 21:49:11 +0100

    Mise a jour class sensor ZhengLi

--- model/sensor/Eye.hpp
+++ model/sensor/Eye.hpp
@@ -1,9 +1,23 @@
 #ifndef _CEye
 #define _CEye
 #include"./Sensor.hpp"
+#include <random>
+#include<iostream>
+using namespace std;
 class Eye: public: Sensor{
 public:
-  Eye( double _dist, double _deg): Sensor( _dist, _deg){
-  }
+	Eye();
+	Eye( double _dist_min, double _dist_max, double _deg_min, double _deg_max, double _det_min,  double _det_max): Sensor(_dist_min, _dist_max, _deg_min, _deg_max, _det_min, _det_max){
+	}
+	double getDistanceEye(){
+		return this->distance;
+	}
+
+	double getDetectionEye(){
+		return this->detection;
+	}
+	double getSDegreeEye(){
+		return this->degree;
+	}
 };
 #endif
