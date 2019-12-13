d7e23d36d1716bffda787d999f1b940b6a38ef04
Daniel Santos <dfsantosbu@unal.edu.co>
2019-12-13 13:54:10 +0100

    Add enum types, and solve return logic bug.

--- logic/factory/BehaviourFactory.hpp
+++ logic/factory/BehaviourFactory.hpp
@@ -10,28 +10,28 @@
 
 class BehaviourFactory{
   private:
-    map<int, shared_ptr<Behaviour>> BehaviourPool;	
+    map<int, shared_ptr<Behaviour>> BehaviourPool;
   public:
   	BehaviourFactory() {}
     shared_ptr<Behaviour> getComportement(int indexCom){
-  	if (!BehaviourPool.count(indexCom)){
-		switch(indexCom){
-			case 0:
-				BehaviourPool[indexCom] = make_shared<FarsightedB>();
-				break;
-			case 1: 
-				BehaviourPool[indexCom] = make_shared<GregariusB>();
-				break;
-			case 2: 
-				BehaviourPool[indexCom] = make_shared<KamikazeB>();
-				break;
-			case 3: 
-				BehaviourPool[indexCom] = make_shared<LazyB>();
-				break;		
-		}		
-    } else {
+    if( indexCom == MultipleBehaviour ) indexCom = 0;
+  	if ( !BehaviourPool.count(indexCom)){
+  		switch(indexCom){
+  			case FarSighted:
+  				BehaviourPool[indexCom] = make_shared<FarsightedB>();
+  				break;
+  			case Gregariu:
+  				BehaviourPool[indexCom] = make_shared<GregariusB>();
+  				break;
+  			case Kamikaze:
+  				BehaviourPool[indexCom] = make_shared<KamikazeB>();
+  				break;
+  			case Lazy:
+  				BehaviourPool[indexCom] = make_shared<LazyB>();
+  				break;
+  		}
+    }
 		return BehaviourPool[indexCom];
-	}}
-
+	}
 };
 #endif
