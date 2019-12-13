71b5fee1ca6d11238a22a587bf0c389506f498c0
gonzaq94 <gonza.quintana94@gmail.com>
2019-12-13 18:27:56 +0100

    general tests file

---
+++ test/Tests.cpp
@@ -0,0 +1,18 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include<bits/stdc++.h>
+using namespace std;
+#include"../logic/include.hpp"
+#include"../model/beast/Beast.hpp"
+#include"../model/behaviour/Lazy.hpp"
+#include"../include/doctest.h"
+#include"../logic/include.hpp"
+#include"../logic/controller/Environment.hpp"
+#include"../model/behaviour/Kamikaze.hpp"
+
+
+
+// Run all tests
+
+//#include"TestBehaviourLazy.cpp"
+
+#include"TestBehaviourKamikaze.cpp"
