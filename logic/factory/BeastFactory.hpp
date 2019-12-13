ecabb7db263444d9a7f6db398ce510ebad8b7574
Daniel Santos <dfsantosbu@unal.edu.co>
2019-12-13 20:11:16 +0100

    Delete old tests.

--- logic/factory/BeastFactory.hpp
+++ logic/factory/BeastFactory.hpp
@@ -25,7 +25,6 @@ class BeastFactory{
       instance = new BeastFactory(behaviourFactory);
       return instance;
     }
-
     Beast* newRandomBeast(int id, int type){
       Beast *b = new Beast(id, {this->rnd->getInt(1, WIDTH_WINDOW),
                             this->rnd->getInt(1, HEIGHT_WINDOW)},
